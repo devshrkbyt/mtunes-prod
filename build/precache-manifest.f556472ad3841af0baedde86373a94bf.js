995e9cf3070977b971894226dee08fcc477ed85f
devshrkbyt <56916929+devshrkbyt@users.noreply.github.com>
2019-10-24 11:35:37 +0200

    Add files via upload

---
+++ build/precache-manifest.f556472ad3841af0baedde86373a94bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c0cef15ccff4e72c23a562d8127234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29640916fba5b05f43a1",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a007d5cd348566bb68f6",
+    "url": "/static/js/2.830ba99c.chunk.js"
+  },
+  {
+    "revision": "29640916fba5b05f43a1",
+    "url": "/static/js/main.fed60c55.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "cfac0d31d64727c93785b6d253872afd",
+    "url": "/static/media/spinner.cfac0d31.gif"
+  }
+]);
